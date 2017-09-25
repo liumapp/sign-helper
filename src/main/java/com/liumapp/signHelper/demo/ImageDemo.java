1d1c9300114cc39c5851ebe1aabdd0ceb2995412
liumapp <liumapp.com@gmail.com>
2017-09-25 11:12:54 +0800

    add image to pdf

---
+++ src/main/java/com/liumapp/signHelper/demo/ImageDemo.java
@@ -0,0 +1,52 @@
+package com.liumapp.signHelper.demo;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import javax.swing.text.Document;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by liumapp on 9/25/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@Component
+public class ImageDemo {
+
+    public static String dataPath = "/usr/local/tomcat/project/pfxSigner/data";
+
+    public static void main(String[] args) {
+
+        PdfStamper stamper = null;
+        try {
+            PdfReader reader = new PdfReader(dataPath + "/test.pdf");
+            stamper = new PdfStamper(reader , new FileOutputStream(dataPath + "/output.pdf"));
+            PdfContentByte over = stamper.getOverContent(1);
+            Image image = Image.getInstance(dataPath +"/sign.png");
+            image.setAbsolutePosition(0 , 0);
+            image.scaleToFit(300 , 109);
+            over.addImage(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stamper.close();
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
